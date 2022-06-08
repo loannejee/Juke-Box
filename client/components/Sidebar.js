d8012cdbc73357019f1c58a9e7f432cadc2a5f93
Joanne <jl7105@nyu.edu>
2022-06-08 13:25:48 -0400

    deselectAlbum function and prop

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,14 +1,17 @@
 // Milestone 3: Render All Albums
+// Milestone 4: Render Single Album View
 
 import React from 'react';
 
-function Sidebar() {
+function Sidebar(props) {
+    const deselectAlbum = props.deselectAlbum
+
     return (
         <div id='sidebar'>
             <img src='juke.svg' id='logo' />
             <section>
                 <h4>
-                    <a>ALBUMS</a>
+                    <a onClick={() => deselectAlbum()}>ALBUMS</a>
                 </h4>
             </section>
         </div>
